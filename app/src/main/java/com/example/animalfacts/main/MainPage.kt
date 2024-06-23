08639ee5e2258fbfbace33587d694d2feda106be
jorgematiasyam <jorgematiasyam@gmail.com>
2024-06-22 22:59:07 -0300

    Agrego botones y mensajes

--- app/src/main/java/com/example/animalfacts/main/MainPage.kt
+++ app/src/main/java/com/example/animalfacts/main/MainPage.kt
@@ -1,9 +1,13 @@
 package com.example.animalfacts.main
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.animalfacts.ui.theme.AnimalFactsTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -19,11 +23,42 @@ fun MainPage(modifier: Modifier = Modifier) {
 
 @Composable
 fun MainView(modifier: Modifier = Modifier, estado: MainEstado, ejecutar: (MainIntencion) -> Unit){
+    Column(modifier = modifier.padding(20.dp)) {
+        when (estado) {
+            is MainEstado.Correcto -> MainViewCorrecto()
+            is MainEstado.Error -> MainViewError()
+            is MainEstado.Cargando -> MainViewCargando()
+
+        }
+            Button(onClick = { ejecutar(MainIntencion.Refrescar) }) {
+                Text(text = "Refrescar")
+            }
+                Button (onClick = { ejecutar(MainIntencion.RomperTodo) }) {
+                Text(text = "Romper todo")
+
+        }
+    }
+
     Text(
         modifier = modifier,
         text = "Hello",
 
     )
+}
+
+@Composable
+fun MainViewCorrecto(){
+    Text(text = "Correcto")
+}
+
+@Composable
+fun MainViewCargando(){
+    Text(text = "Cargando")
+}
+
+@Composable
+fun MainViewError(){
+    Text(text = "Error")
 }
 
 
